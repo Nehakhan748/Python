0e80a5b06e6d373b6e8c30736f0968e10128b4eb
Nehakhan748 <nehamaryyam.khan@gmail.com>
2025-07-12 14:07:39 +0500

    Word Reverser

---
+++ code8.py
@@ -0,0 +1,12 @@
+       #"""Word Reverser"""
+
+#Input a sentence:
+sentence = input("Enter a sentence: ")
+#split each word ny given sentence:
+words = sentence.split()
+#for loop :
+for word in words:
+   print(word[::-1])   #reverse each words in sentence:
+
+
+
